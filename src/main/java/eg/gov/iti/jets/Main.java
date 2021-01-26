3b4611fe7d96ee12a7778070373d3ffadd6453b3
MostafaYahia <mostafayehyax23@gmail.com>
2021-01-26 23:29:54 +0200

    Initial commit

---
+++ src/main/java/eg/gov/iti/jets/Main.java
@@ -0,0 +1,28 @@
+package eg.gov.iti.jets;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.show();
+    }
+
+    @Override
+    public void init() {
+        // Initialize Database & Network Connections
+    }
+
+    @Override
+    public void stop() {
+        // Terminate Database & Network Connections
+    }
+
+}
