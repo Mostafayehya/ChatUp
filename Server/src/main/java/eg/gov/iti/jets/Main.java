b30a379c53bfe9cb9931f4aeefd8839501205612
MostafaYahia <mostafayehyax23@gmail.com>
2021-01-30 20:26:38 +0200

    Refactor to use dedicated maven modules for Client and Server

---
+++ Server/src/main/java/eg/gov/iti/jets/Main.java
@@ -0,0 +1,31 @@
+package eg.gov.iti.jets;
+
+import eg.gov.iti.jets.utilities.StageCoordinator;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        StageCoordinator stageCoordinator=StageCoordinator.getInstance();
+        stageCoordinator.setStage(primaryStage);
+        //Call first page function
+        primaryStage.show();
+    }
+
+    @Override
+    public void init() {
+        // Initialize Database & Network Connections
+    }
+
+    @Override
+    public void stop() {
+        // Terminate Database & Network Connections
+    }
+
+}
