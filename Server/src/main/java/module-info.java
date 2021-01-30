fe9e50fc49ee05436d979121c1792237882d4dc5
MostafaYahia <mostafayehyax23@gmail.com>
2021-01-30 20:47:48 +0200

    Move src to client and server directory

---
+++ Server/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ChatUp.Client {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome5;
+    requires org.kordamp.ikonli.materialdesign2;
+
+    exports eg.gov.iti.jets;
+    opens eg.gov.iti.jets.ui.controllers;
+}
