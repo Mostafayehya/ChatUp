3c0f4af31b746f736b07589e5779e07e8ad4e9e1
MostafaYahia <mostafayehyax23@gmail.com>
2021-02-02 21:26:23 +0200

    Add seperate navigation fxml file

---
+++ Client/src/main/java/eg/gov/iti/jets/ui/controllers/navigationbarControlller.java
@@ -0,0 +1,25 @@
+package eg.gov.iti.jets.ui.controllers;
+
+import javafx.fxml.FXML;
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import org.kordamp.ikonli.javafx.FontIcon;
+
+public class navigationbarControlller {
+
+    @FXML
+    private JFXButton profileButton;
+
+    @FXML
+    private JFXButton chatsButton;
+
+    @FXML
+    private JFXButton contactsButton;
+
+    @FXML
+    private JFXButton notificationButton;
+
+    @FXML
+    private JFXButton invitationsButton;
+
+}
