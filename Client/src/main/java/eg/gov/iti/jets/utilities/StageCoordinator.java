a62349d2d4f8bbfca51b2414b2d3861defe9f9b9
MostafaYahia <mostafayehyax23@gmail.com>
2021-02-04 21:59:13 +0200

    Make the app works with initial screen of chat page

--- Client/src/main/java/eg/gov/iti/jets/utilities/StageCoordinator.java
+++ Client/src/main/java/eg/gov/iti/jets/utilities/StageCoordinator.java
@@ -1,7 +1,11 @@
 package eg.gov.iti.jets.utilities;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,6 +34,30 @@ public class StageCoordinator {
     }
 
     public void switchToChatScreen() {
+
+        if (stage ==null){
+            throw new RuntimeException("Stage Coordinator must be assigned a stage before being able to use it");
+        }
+
+        if (!scenes.containsKey("chat")) {
+            try {
+                System.out.println("Created New Scene");
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/ChatPage.fxml"));
+                Parent chatView = fxmlLoader.load();
+                Scene chatScene = new Scene(chatView);
+                SceneData loginSceneData = new SceneData(fxmlLoader, chatView, chatScene);
+                scenes.put("chat", loginSceneData);
+                stage.setScene(chatScene);
+            } catch (IOException e) {
+                System.out.println("IO Exception: Couldn't load 'Chat Page' FXML file");
+            }
+        } else {
+            System.out.println("Loaded Existing Scene");
+            SceneData loginSceneData = scenes.get("chat");
+            Scene loginScene = loginSceneData.getScene();
+            stage.setScene(loginScene);
+        }
+
     }
     //add public function to load each age like public void goToLoginPage();
 }
