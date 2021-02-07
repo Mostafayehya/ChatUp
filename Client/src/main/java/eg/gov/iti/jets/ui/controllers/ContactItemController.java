997710207108c9197849a770c214b1509f0f5ef7
Hadeer <hadeerelnaggar@hotmail.com>
2021-02-07 23:28:40 +0200

    added contact Class and list and list item controllers

--- Client/src/main/java/eg/gov/iti/jets/ui/controllers/ContactItemController.java
+++ Client/src/main/java/eg/gov/iti/jets/ui/controllers/ContactItemController.java
@@ -1,4 +1,32 @@
 package eg.gov.iti.jets.ui.controllers;
 
-public class ContactItemController {
+import eg.gov.iti.jets.ui.models.Contact;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
+import org.kordamp.ikonli.javafx.FontIcon;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ContactItemController implements Initializable {
+    @FXML
+    Circle imageCircle;
+    @FXML
+    Label contactNameLabel;
+    @FXML
+    Label bioLabel;
+    @FXML
+    FontIcon messageIcon;
+    Contact contact;
+
+    public ContactItemController(Contact contact){
+        this.contact = contact;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- Client/src/main/java/eg/gov/iti/jets/ui/controllers/ContactsListPageController.java
+++ Client/src/main/java/eg/gov/iti/jets/ui/controllers/ContactsListPageController.java
@@ -1,4 +1,31 @@
 package eg.gov.iti.jets.ui.controllers;
 
-public class ContactsListPageController {
+import eg.gov.iti.jets.ui.models.Contact;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.shape.Circle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ContactsListPageController implements Initializable {
+    @FXML
+    ListView<Contact> contactsListView;
+    ObservableList<Contact> contactObservableList;
+
+    public ContactsListPageController(){
+        contactObservableList = FXCollections.observableArrayList(
+                new Contact("019922","hadeer","this is my bio","hadeer@gmail.com","/photos/user.jpg"),
+                new Contact("8473992","yasmina","this is yasmina's bio","yasmina@gmail.com","/photos/user.jpg")
+        );
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
