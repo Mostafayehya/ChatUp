09aac27d0e2a45385bfc5864d56e480af06519fb
Hadeer <hadeerelnaggar@hotmail.com>
2021-02-11 16:25:37 +0200

    Add create new contact button in contacts list page

--- Client/src/main/java/eg/gov/iti/jets/Main.java
+++ Client/src/main/java/eg/gov/iti/jets/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
         stageCoordinator.setStage(primaryStage);
         //Call first page function
         primaryStage.setTitle("ChatUp - Client");
+        stageCoordinator.gotoContactsListPage();
         primaryStage.show();
     }
 
