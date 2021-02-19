cafcb4aefd41be33ae3b4023bbd52f5d4accea86
MostafaYahia <mostafayehyax23@gmail.com>
2021-02-19 22:38:37 +0200

    Add a small todo

--- Client/src/main/java/eg/gov/iti/jets/Main.java
+++ Client/src/main/java/eg/gov/iti/jets/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+// todo) make the merge
 public class Main extends Application {
     RMIManager rmiManager;
     public static void main(String[] args) {
