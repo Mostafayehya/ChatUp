62d148ddfedc1a69496c5338bd1beb1bcb3c4663
yasmina <yasminaabdelazim@gmail.com>
2021-02-21 05:07:35 +0200

    make data appear dynamically by every radio button check fix (male/female) issue try adding tiptool (still not working) refactor statistics in a separate class in the utilities

--- Server/src/main/java/eg/gov/iti/jets/ui/controllers/serverDashBoardController.java
+++ Server/src/main/java/eg/gov/iti/jets/ui/controllers/serverDashBoardController.java
@@ -1,11 +1,13 @@
 package eg.gov.iti.jets.ui.controllers;
 
+import domains.Status;
 import eg.gov.iti.jets.Main;
 import eg.gov.iti.jets.data.DataBaseConnection;
 import eg.gov.iti.jets.data.dao.UserDao;
 import eg.gov.iti.jets.data.dao.UserDaoImpl;
 import eg.gov.iti.jets.io.Server;
 import eg.gov.iti.jets.services.implementations.AuthenticationServiceImpl;
+import eg.gov.iti.jets.utilities.statistics;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -14,6 +16,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import services.AuthenticationService;
@@ -31,32 +34,19 @@ import java.util.ResourceBundle;
 public class serverDashBoardController implements Initializable { //
     DataBaseConnection dataBaseConnection;
     Server server;
-    UserDao d = new UserDaoImpl();
+
     @FXML
     private Button startbutton, stopbutton;
-    private PieChart mypiechart = new PieChart();
-    private PieChart mypiechart2 = new PieChart();
-    XYChart.Series dataSeries1 = new XYChart.Series();
-    CategoryAxis xAxis = new CategoryAxis();
-    NumberAxis yAxis = new NumberAxis();
-    ObservableList<PieChart.Data> gender;
-    ObservableList<PieChart.Data> onoff;
-    private BarChart mychart = new BarChart(xAxis, yAxis);
     @FXML
     private Pane mychartpane;
-
-    ResultSet rs1 = d.getAllByCountry();
-    ResultSet rs2 = d.getAllByGender();
-    ResultSet rs3 = d.getAllOnOff();
+    statistics statistics = new statistics();
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
         dataBaseConnection = DataBaseConnection.getInstance();
         server = Server.getInstance();
-        getcountrydata();
-        getgenderdata();
-        getstatusdata();
-
-
+        statistics.getcountrydata();
+        statistics.getgenderdata();
+        statistics.getstatusdata();
     }
 
     @FXML
@@ -86,94 +76,53 @@ public class serverDashBoardController implements Initializable { //
     }
 
     public void showGender() {
-       // getgenderdata();
-
+        try {
+            statistics.rs2.first();
+            statistics.rs2.previous();
+        } catch (Exception e) {
+        }
+        statistics.getgenderdata();
         try {
             mychartpane.getChildren().clear();
         } catch (Exception e) {
         }
 
-        mypiechart.setPrefWidth(300);
-        mypiechart.setPrefHeight(400);
-        mychartpane.getChildren().add(mypiechart);
+        statistics.mypiechart.setPrefWidth(300);
+        statistics.mypiechart.setPrefHeight(400);
+        mychartpane.getChildren().add(statistics.mypiechart);
     }
 
     public void showCountries() {
-       // getcountrydata();
+
+        try {
+            statistics.rs1.first();
+            statistics.rs1.previous();
+        } catch (Exception e) {
+        }
+        statistics.getcountrydata();
         try {
             mychartpane.getChildren().clear();
         } catch (Exception e) {
         }
-        mychart.setPrefWidth(300);
-        mychart.setPrefHeight(400);
-        mychartpane.getChildren().add(mychart);
+        statistics.mychart.setPrefWidth(300);
+        statistics.mychart.setPrefHeight(400);
+        mychartpane.getChildren().add(statistics.mychart);
     }
 
     public void showOnOff() {
-        //getstatusdata();
+        try {
+            statistics.rs3.first();
+            statistics.rs3.previous();
+        } catch (Exception e) {
+        }
+        statistics.getstatusdata();
         try {
             mychartpane.getChildren().clear();
         } catch (Exception e) {
         }
-        mypiechart2.setPrefWidth(300);
-        mypiechart2.setPrefHeight(400);
-        mychartpane.getChildren().add(mypiechart2);
-    }
-
-    private void getcountrydata() {
-
-        try {
-            while (rs1.next()) {
-                dataSeries1.getData().add(new XYChart.Data<>(rs1.getString("country"), rs1.getInt("Number")));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        mychart.getData().add(dataSeries1);
-    }
-
-    private void getgenderdata() {
-
-        int countmale = 0;
-        int countfemale = 0;
-        try {
-            rs2.next();
-            countmale = rs2.getInt("Number");
-            rs2.next();
-            countfemale = rs2.getInt("Number");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        gender = FXCollections.observableArrayList(
-
-                new PieChart.Data("Female", countmale),
-                new PieChart.Data("Male", countfemale));
-
-        mypiechart.setData(gender);
-
-    }
-
-    private void getstatusdata() {
-
-        int counton = 0;
-        int countoff = 0;
-        try {
-            rs3.next();
-            countoff = rs3.getInt("Number");
-            rs3.next();
-            counton = rs3.getInt("Number");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        onoff = FXCollections.observableArrayList(
-
-                new PieChart.Data("Offline", countoff),
-                new PieChart.Data("Online", counton));
-
-        mypiechart2.setData(onoff);
-
+        statistics.mypiechart2.setPrefWidth(300);
+        statistics.mypiechart2.setPrefHeight(400);
+        mychartpane.getChildren().add(statistics.mypiechart2);
     }
 
 }
